025977b9bd5f433a54ba0f9529ed69b36d9d0c20
Helton Reis <hrkings@rhaizzing.com>
2021-05-24 15:58:46 -0300

    [change] Change the installation to use symbolic links

--- install.sh
+++ install.sh
@@ -1,49 +1,26 @@
 #!/bin/sh
 
 # Define install directories and names
-install_path="$HOME/.pokemonsay"
-bin_path="$HOME/bin"
-pokemonsay_bin="pokemonsay"
-pokemonthink_bin="pokemonthink"
+INSTALL_PATH="$HOME/.bin/pokemonsay"
+BIN_PATH="/bin"
+POKEMONSAY_BIN="pokemonsay"
+POKEMONTHINK_BIN="pokemonthink"
 
 # Make sure the directories exist
-mkdir -p $install_path/
-mkdir -p $install_path/cows/
-mkdir -p $bin_path/
+mkdir -p $INSTALL_PATH/
+mkdir -p $INSTALL_PATH/pokemons/
+mkdir -p $BIN_PATH/
 
 # Copy the cows and the main script to the install path.
-cp ./cows/*.cow $install_path/cows/
-cp ./pokemonsay.sh $install_path/
-cp ./pokemonthink.sh $install_path/
+cp ./pokemons/*.cow $INSTALL_PATH/pokemons/
+cp ./pokemonsay.sh $INSTALL_PATH/
+cp ./pokemonthink.sh $INSTALL_PATH/
 
-# Create the pokemonsay script in the home bin directory.
-cat > $bin_path/$pokemonsay_bin <<- EOF
-	#!/bin/sh
-
-	# This script changes to the pokemonsay installation directory,
-	# runs the main script for running the pokemonsay, and changes
-	# back to the previous directory.
-
-	cd $install_path/
-	./pokemonsay.sh \$@
-	cd - >/dev/null
-EOF
-
-# Create the pokemonthink script in the home bin directory.
-cat > $bin_path/$pokemonthink_bin <<- EOF
-	#!/bin/sh
-
-	# This script changes to the pokemonsay installation directory,
-	# runs the main script for running the pokemonthink, and changes
-	# back to the previous directory.
-
-	cd $install_path/
-	./pokemonthink.sh \$@
-	cd - >/dev/null
-EOF
+sudo ln -sf "${INSTALL_PATH}/${POKEMONSAY_BIN}.sh" "${BIN_PATH}/${POKEMONSAY_BIN}"
+sudo ln -sf "${INSTALL_PATH}/${POKEMONTHINK_BIN}.sh" "${BIN_PATH}/${POKEMONTHINK_BIN}"
 
 # Create uninstall script in the install directory
-cat > $install_path/uninstall.sh <<- EOF
+cat > $INSTALL_PATH/uninstall.sh <<- EOF
 	#!/bin/sh
 
 	#
@@ -51,24 +28,22 @@ cat > $install_path/uninstall.sh <<- EOF
 	#
 
 	# Remove the install directory
-	rm -r "$install_path/"
+	rm -r "$INSTALL_PATH/"
 
-	# Remove the bin files
-	rm "$bin_path/$pokemonsay_bin"
-	rm "$bin_path/$pokemonthink_bin"
+	# Remove the bin files symbolic links
+	sudo rm "$BIN_PATH/$POKEMONSAY_BIN"
+	sudo rm "$BIN_PATH/$POKEMONTHINK_BIN"
 
 	# Say what's going on.
-	echo "'$install_path/' directory was removed."
-	echo "'$bin_path/$pokemonsay_bin' file was removed."
-	echo "'$bin_path/$pokemonthink_bin' file was removed."
+	echo "'$INSTALL_PATH/' directory was removed."
+	echo "'$BIN_PATH/$POKEMONSAY_BIN' file was removed."
+	echo "'$BIN_PATH/$POKEMONTHINK_BIN' file was removed."
 EOF
 
 # Change permission of the generated scripts
-chmod +x "$bin_path/$pokemonsay_bin"
-chmod +x "$bin_path/$pokemonthink_bin"
-chmod +x "$install_path/uninstall.sh"
+chmod +x "$INSTALL_PATH/uninstall.sh"
 
-echo "The files were installed to '$install_path/'."
-echo "A '$pokemonsay_bin' script was created in '$bin_path/'."
-echo "A uninstall script was created in '$install_path/'."
-echo "It may be necessary to logout and login back again in order to have the '$pokemonsay_bin' available in your path."
+echo "The files were installed to '$INSTALL_PATH/'."
+echo "A '$POKEMONSAY_BIN' script was created in '$BIN_PATH/'."
+echo "A uninstall script was created in '$INSTALL_PATH/'."
+echo "It may be necessary to logout and login back again in order to have the '$POKEMONSAY_BIN' available in your path."
