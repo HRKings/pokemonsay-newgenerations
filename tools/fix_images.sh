d474ede309be146692685c4081dee599e4d14c93
Helton Reis <heltonkreis@gmail.com>
2021-10-04 20:25:21 -0300

    [chore]{!!!} Get sprites from the PokeSprite project

--- tools/fix_images.sh
+++ tools/fix_images.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-for image in ./scrapped-data/*.png
+for image in ./sprites/*.png
 do
-	# Mirror the image in the horizontal direction so
-	# that the pokemon will be looking to the right.
-	convert -flop "$image" "$image"
 	# Trim the useless empty space around the pokemon.
 	convert -trim "$image" "$image"
 done
